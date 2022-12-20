4b581d49ce3135d7089492b983844bb8006bbad3
Kevin Egan <kevinegan31@gmail.com>
2022-12-20 08:53:46 -0600

    Create GROUP_BY_Challenge.sql

---
+++ GROUP_BY_Statements/GROUP_BY_Challenge.sql
@@ -0,0 +1,36 @@
+/*
+Challenge No.1
+We have two staff members, with Staff IDs 1 and 2.
+We want to give a bonus to the staff member that handled the most payments.
+(Most in terms of number of payments processed, not total dollar amount).
+How many payments did each staff member handle and who gets the bonus?
+*/
+SELECT staff_id, COUNT(staff_id)
+FROM payment
+WHERE staff_id IN (1,2)
+GROUP BY staff_id;
+/*
+Answer: Staff #2 gets the bonus
+*/
+
+/*
+Challenge No.2
+Corporate HQ is conducting a study on the relationship between replacement cost
+and a movie MPAA rating (e.g. G, PG, R, etc...)
+What is the average replacement cost per MPAA rating?
+Note: You may need to expand the AVG column to view correct results.
+*/
+SELECT rating, ROUND(AVG(replacement_cost), 2)
+FROM film
+GROUP BY rating;
+
+/*
+Challenge No.3
+We are running a promotion to reward our top 5 customers with coupons.
+What are the customer ids of the top 5 customers by total spend?
+*/
+SELECT customer_id, SUM(amount) as total_spend
+FROM payment
+GROUP BY customer_id
+ORDER BY total_spend DESC
+LIMIT 5;
